bc1d0486e1476e95932dc1de1f38a15f7566e874
Ryan Fink <ryanjfink@gmail.com>
2016-02-23 14:13:49 -0500

    Use csv-parse, in case fields have commas or weird characters

--- test/awql.test.js
+++ test/awql.test.js
@@ -2,6 +2,7 @@
 
 var should = require('should');
 var adwords = require('..');
+var Awql = require('../lib/awql');
 var opts = require('./test-auth.json');
 
 /*global describe*/
@@ -39,6 +40,39 @@ describe('AWQL tests', function() {
         return done();
       })
       .catch(done);
+  });
+
+  it('should parse correctly with weird characters', function(done) {
+    var parser = new Awql({}, null);
+    var data = [
+      '"CAMPAIGN_PERFORMANCE_REPORT (Feb 22, 2016)"',
+      'Search Lost IS (budget),Total cost,Campaign ID,Campaign,Day',
+      '--,3310000,838383833,"General Terms (Q1, \'16)",2016-02-22',
+      '--,13900000,838383834,"Mens Collection (Q1, \'16)",2016-02-22',
+      '--,3020000,838383835,Brand Terms,2016-02-22',
+      'Total,20230000, --, --, --',
+      ''
+    ].join('\n');
+    var res = parser.parse(data);
+    should.exist(res);
+    res.should.have.property('data');
+    res.data.should.have.length(3);
+    res.data[0].should.have.property('Search Lost IS (budget)', '--');
+    res.data[0].should.have.property('Total cost', '3310000');
+    res.data[0].should.have.property('Campaign ID', '838383833');
+    res.data[0].should.have.property('Campaign', 'General Terms (Q1, \'16)');
+    res.data[0].should.have.property('Day', '2016-02-22');
+    res.data[1].should.have.property('Search Lost IS (budget)', '--');
+    res.data[1].should.have.property('Total cost', '13900000');
+    res.data[1].should.have.property('Campaign ID', '838383834');
+    res.data[1].should.have.property('Campaign', 'Mens Collection (Q1, \'16)');
+    res.data[1].should.have.property('Day', '2016-02-22');
+    res.data[2].should.have.property('Search Lost IS (budget)', '--');
+    res.data[2].should.have.property('Total cost', '3020000');
+    res.data[2].should.have.property('Campaign ID', '838383835');
+    res.data[2].should.have.property('Campaign', 'Brand Terms');
+    res.data[2].should.have.property('Day', '2016-02-22');
+    return done();
   });
 });
 
